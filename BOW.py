80ea70ab31caf16f8fe25af70046f49099aaffb8
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2021-11-16 16:09:56 -0700

    Initial Commit

---
+++ BOW.py
@@ -0,0 +1,9 @@
+'''
+
+BAG OF WORDS IMPLEMENTATION (BASIC)
+
+ -- Creates the BOW vector from training data
+ -- Grabs the BOW feature vector from testing data
+ -- Uses KNN with a few distance metrics to determine closes neighbors
+
+'''
